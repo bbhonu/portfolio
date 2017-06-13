d8a4acd7b21b691de0dba0b660fbeaba6df1f2d4
Hannah Vine <hannahvine@Hannahs-MacBook-Pro-2.local>
2017-06-13 17:04:25 -0600

    omg

---
+++ js/main.js
@@ -0,0 +1,168 @@
+
+
+$(document).ready(function(){
+
+
+
+
+$("#nav").hide();
+
+// var lastScrollTop = 0;
+// $(window).scroll(function(event){
+//    var st = $(this).scrollTop();
+//    if (st > lastScrollTop){
+//    	    	$("#starleft").animate({left:"-3.11vw",width:"4vw"
+//     		// top:"300px"
+//     },{duration:600});
+//     	$("#starright").animate({right:"-3.11vw",width:"4vw"
+//     		// top:"300px"
+//     },{duration:600});
+
+
+$("#plus").click(function(){
+$("#nav").slideToggle();
+});
+
+
+
+    	// $("#starleft").css("width","7vw");
+    	// $("#starright").css("width","7vw");
+
+
+   //  	$("#moveup").animate({ marginTop:"-70px"
+   //  	},{duration:600});
+
+   //  	$("#canvas-1").animate({ height:"0px"
+   //  	},{duration:600});
+   //     // downscroll code
+   // }
+   // lastScrollTop = st;
+
+// });
+
+
+
+
+});
+
+
+var granimInstance = new Granim({
+    element: '#canvas-1',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ]
+        }
+    }
+});
+
+var granimInstance = new Granim({
+    element: '#canvas-2',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1],
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#AA076B', '#61045F'],
+                ['#02AAB0', '#00CDAC'],
+                ['#DA22FF', '#9733EE']
+            ]
+        }
+    }
+});
+
+
+// $('#projects').waypoint(function(event, direction) {
+//     if (direction === 'down') {
+//     			$("#sectionnav").css("position","fixed");
+// 		$("#sectionnav").css("transform","rotate(-90deg) translate(0px,-360px)");
+
+       
+//     } else {
+
+//         		$("#sectionnav").css("position","absolute");
+
+//     }
+// });
+
+  	    			$("#sectionnav").hide();
+
+
+$('#projects').waypoint(function(direction) {
+  if (direction === 'down') {
+  	    			$("#sectionnav").fadeIn(1000);
+
+    			 $("#sectionnav").css("position","fixed");
+
+		// $("#sectionnav").css("transform","rotate(-90deg) translate(0vw,-30vw)");
+  }
+});
+
+$('#projects').waypoint(function(direction) {
+  if (direction === 'up') {
+        		$("#sectionnav").fadeOut();
+        				// $("#sectionnav").css("transform","rotate(-90deg) translate(-31vw, -47vw)");
+
+  }
+}, {
+  offset: '-20%'
+});
+
+
+// var waypoint = new Waypoint({
+// 	element: document.getElementById("projects"),
+// 	handler: function(down) {
+// 		$("#sectionnav").css("position","fixed");
+// 		$("#sectionnav").css("transform","rotate(-90deg) translate(0px,-360px)");
+// 		// $("#sectionnav").css("left","200px;")
+// 	}
+// });
+
+
+// var waypoint1 = new Waypoint({
+// 	element: document.getElementById("top"),
+// 	handler: function(up) {
+// 		$("#sectionnav").css("display","none");
+
+// 	},
+// 	offset: 300
+// });
+
+
+// var waypoint2 = new Waypoint({
+// 	element: document.getElementById("projects"),
+// 	handler: function(up) {
+// 		$("#sectionnav").css("display","none");
+// 		// $("#sectionnav").css("left","200px;")
+// 	}
+// });
+
+	// $(window).on("scroll",function(e){
+	// 	if(this.scrollTop>1000) {
+	// 		$("#sectionnav").css("position","fixed");
+	// 					$("#sectionnav").css("left","150px");
+
+	// 	}
+	// 	else {
+	// 		$("#sectionnav").css("position","absolute");
+	// 	}
+	// });
+
+	// $(window).scroll(function(event){
+ //   if(this.scrollTop>600) {
+	// 		$("s").addClass("fixme");
+
+	// 	}
+	// 	else {
+	// 		sidebar.removeClass("fixme");
+	// 	}
+	// });
