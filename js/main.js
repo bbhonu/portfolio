f023c7cf126a6dc9995fb6c3bb51b6f2e338ae45
Hannah Vine <hannahvine@Hannahs-MacBook-Pro-2.local>
2017-06-15 17:03:09 -0600

    yas

--- js/main.js
+++ js/main.js
@@ -2,8 +2,31 @@ $(document).ready(function() {
 
 
 
-
     $("#nav").hide();
+    $(".head").hide();
+    $("#introright").hide();
+    $("#introleft").hide();
+    $("#canvas-5").hide();
+    $("#a").hide();
+    $("#b").hide();
+    $("#skillz").hide();
+
+        $(".head").show().delay(3000).animate({marginTop:"0px"},2000);
+
+        // $(".head").animate({opacity:"1"},2000);
+
+
+$("#a").delay(1000).fadeIn(1000);
+$("#b").delay(1500).fadeIn(1000);
+$("#skillz").delay(2000).fadeIn().animate({left:"10px"},2000);
+
+    $("#introleft").fadeIn(1000);
+
+    $("#introright").delay(2000).fadeIn(1000);
+    $("#canvas-5").delay(2000).fadeIn(1000);
+
+
+
 
     // var lastScrollTop = 0;
     // $(window).scroll(function(event){
@@ -63,7 +86,7 @@ var granimInstance = new Granim({
     }
 });
 
-var granimInstance = new Granim({
+var granimInstance2 = new Granim({
     element: '#canvas-2',
     name: 'basic-gradient',
     direction: 'left-right',
@@ -80,7 +103,7 @@ var granimInstance = new Granim({
     }
 });
 
-var granimInstance = new Granim({
+var granimInstance3 = new Granim({
     element: '#canvas-3',
     name: 'basic-gradient',
     direction: 'left-right',
@@ -96,7 +119,7 @@ var granimInstance = new Granim({
         }
     }
 });
-var granimInstance = new Granim({
+var granimInstance4 = new Granim({
     element: '#canvas-4',
     name: 'basic-gradient',
     direction: 'left-right',
@@ -109,6 +132,23 @@ var granimInstance = new Granim({
                 ['#02AAB0', '#00CDAC'],
                 ['#DA22FF', '#9733EE']
             ]
+        }
+    }
+});
+
+var granimInstance5 = new Granim({
+    element: '#canvas-5',
+    name: 'basic-gradient',
+    direction: 'left-right',
+    opacity: [1, 1, 1],
+    isPausedWhenNotInView: true,
+    states: {
+        "default-state": {
+            gradients: [
+                ['#e4a6d8', '#dadff3','#f3c7bc'],
+                ['#ffffff', '#e064c8',"#ff9d85"],
+            ],
+            transitionSpeed: "1000"
         }
     }
 });
@@ -136,6 +176,7 @@ $("#resumenav").css("position", "fixed");
 $('#projects').waypoint(function(direction) {
     if (direction === 'down') {
         $("#projectsnav").fadeIn(1000);
+        $("#projlink").css("color","white");
         // $("#sidetext").html("section one");
 
 
@@ -145,28 +186,17 @@ $('#projects').waypoint(function(direction) {
 });
 
 
-// $('#project3').waypoint(function(direction) {
-//   if (direction === 'down') {
-//               $("#projectsnav").hide();
-//               // $("#sidetext").html("section one");
-
-
-//            $("#projectsnav").css("position","static");
-
-//     // $("#sectionnav").css("transform","rotate(-90deg) translate(0vw,-30vw)");
-//   }
-// },{
-//   offset:"50%"
-// });
 
 $('#projects').waypoint(function(direction) {
     if (direction === 'up') {
         $("#projectsnav").fadeOut();
+                $("#projlink").css("color","black");
+
         // $("#sectionnav").css("transform","rotate(-90deg) translate(-31vw, -47vw)");
 
     }
 }, {
-    offset: '-20%'
+    offset: '10%'
 });
 
 
@@ -177,6 +207,8 @@ $('#resume').waypoint(function(direction) {
         $("#projectsnav").hide();
         // $("#projectsnav").css("display", "none");
         $("#projectstext").css("display","none");
+                $("#projlink").css("color","black");
+        $("#resumelink").css("color","white");
 
 
 
@@ -199,6 +231,10 @@ $('#resume').waypoint(function(direction) {
         $("#resumenav").hide();
         $("#projectsnav").show();
                 $("#projectstext").css("display","block");
+                        $("#projlink").css("color","white");
+                                $("#resumelink").css("color","black");
+
+
 
 
 
@@ -211,33 +247,6 @@ $('#resume').waypoint(function(direction) {
   offset:'50%'
 });
 
-// var waypoint = new Waypoint({
-//  element: document.getElementById("projects"),
-//  handler: function(down) {
-//    $("#sectionnav").css("position","fixed");
-//    $("#sectionnav").css("transform","rotate(-90deg) translate(0px,-360px)");
-//    // $("#sectionnav").css("left","200px;")
-//  }
-// });
-
-
-// var waypoint1 = new Waypoint({
-//  element: document.getElementById("top"),
-//  handler: function(up) {
-//    $("#sectionnav").css("display","none");
-
-//  },
-//  offset: 300
-// });
-
-
-// var waypoint2 = new Waypoint({
-//  element: document.getElementById("projects"),
-//  handler: function(up) {
-//    $("#sectionnav").css("display","none");
-//    // $("#sectionnav").css("left","200px;")
-//  }
-// });
 
 // $(window).on("scroll",function(e){
 //  if(this.scrollTop>1000) {
